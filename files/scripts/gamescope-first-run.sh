699d8b729f1d01d80cba36ec6ddab4e99b9dabb9
Mohamed Chrayed <Mo C>
2025-09-27 12:01:47 +0200

    fix script

--- files/scripts/gamescope-first-run.sh
+++ files/scripts/gamescope-first-run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-wget https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz -O %{buildroot}%{_datadir}/gamescope-session-plus/bootstraplinux_ubuntu12_32.tar.xz
-mkdir -p %{_sysconfdir}/skel/.local/share/Steam
-tar -xf %{_datadir}/gamescope-session-plus/bootstraplinux_ubuntu12_32.tar.xz -C %{_sysconfdir}/skel/.local/share/
-
+curl --retry 3 -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
