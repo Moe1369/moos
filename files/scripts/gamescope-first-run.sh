f5775b8f9e1da44d84c3d080b776acde159e8e0d
Mo <65728018+Moe1369@users.noreply.github.com>
2025-09-27 22:33:18 +0200

    Delete files/scripts/gamescope-first-run.sh

--- files/scripts/gamescope-first-run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p /usr/share/gamescope-session-plus/
-curl --retry 3 -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
