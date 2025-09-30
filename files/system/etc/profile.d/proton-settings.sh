e302e9a03af3c32be88aebb7fca2c1924343c1e0
Mo <65728018+Moe1369@users.noreply.github.com>
2025-09-30 10:23:31 +0200

    Update proton-settings.sh

--- files/system/etc/profile.d/proton-settings.sh
+++ files/system/etc/profile.d/proton-settings.sh
@@ -1,5 +1,5 @@
 # Enable Proton Stuff only on gnome. Leave as default for console setup
-if [[ "$DESKTOP_SESSION" == "gnome*" ]]; then
+if [[ "$DESKTOP_SESSION" == "gnome" ]]; then
   export PROTON_FSR4_UPGRADE=1
   export PROTON_ENABLE_HDR=1
   export PROTON_ENABLE_WAYLAND=1
