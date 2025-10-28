1c8f768bb93af8bb57fbd41d3964f5cb45e906c2
Mo <65728018+Moe1369@users.noreply.github.com>
2025-10-28 19:47:15 +0100

    Update proton-settings.sh

--- files/system/etc/profile.d/proton-settings.sh
+++ files/system/etc/profile.d/proton-settings.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if [ "$XDG_CURRENT_DESKTOP" = "KDE" ] || [[ "$XDG_CURRENT_DESKTOP" == *"Plasma"* ]]; then
-    export PROTON_FSR4_UPGRADE=1
-    export PROTON_ENABLE_HDR=1
-    export PROTON_ENABLE_WAYLAND=1
-fi
+export PROTON_FSR4_UPGRADE=1
+export PROTON_ENABLE_HDR=1
+export PROTON_ENABLE_WAYLAND=1
