dab5edbe895f914dd355d8d9fe3185a24b563037
Mo <65728018+Moe1369@users.noreply.github.com>
2025-12-12 10:29:17 +0100

    Update proton-settings.sh

--- files/system/etc/profile.d/proton-settings.sh
+++ files/system/etc/profile.d/proton-settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PROTON_ENABLE_HDR=1
 export PROTON_ENABLE_WAYLAND=1
-#export PROTON_FSR4_UPGRADE=1
-#export DXIL_SPIRV_CONFIG=wmma_rdna3_workaround
+export DXVK_HDR=1
+export ENABLE_HDR_WSI=1
