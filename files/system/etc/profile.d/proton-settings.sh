053e8ede27338e606d69df8e8851cb7faa82c83f
Mo <65728018+Moe1369@users.noreply.github.com>
2026-02-21 13:48:03 +0100

    Enable HDR WSI, FSR4 upgrade, and DXIL config

--- files/system/etc/profile.d/proton-settings.sh
+++ files/system/etc/profile.d/proton-settings.sh
@@ -2,6 +2,6 @@
 export PROTON_ENABLE_HDR=1
 export PROTON_ENABLE_WAYLAND=1
 export DXVK_HDR=1
-#export ENABLE_HDR_WSI=1
-#export PROTON_FSR4_UPGRADE=1
-#export DXIL_SPIRV_CONFIG=wmma_rdna3_workaround
+export ENABLE_HDR_WSI=1
+export PROTON_FSR4_UPGRADE=1
+export DXIL_SPIRV_CONFIG=wmma_rdna3_workaround
